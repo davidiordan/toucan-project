9a6fabc06a6ea6e96e7328e1bfffcc78f0da5561
David Iordan <davidiordan@icloud.com>
2018-12-22 01:35:02 -0600

    Ugly skeleton of frontend

---
+++ frontend/toucan/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Dimensions } from 'react-native';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation';
+import { Font } from 'expo';
+
+import ChatScreen from './ChatScreen';
+import NestScreen from './NestScreen';
+
+const { width } = Dimensions.get('window');
+
+export default class App extends React.Component {
+  state = { loading: true };
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
+      'Ubuntu-B': require('./assets/Fonts/Ubuntu-B.ttf'),
+    });
+    this.setState({ loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <Expo.AppLoading />
+    }
+    return (
+      <LeftDrawerNav  />
+    );
+  }
+}
+
+const CustomDrawerComponent = (props) => (
+  <SafeAreaView style={{flex: 1}}>
+    <ScrollView>
+      <DrawerItems {...props} />
+    </ScrollView>
+  </SafeAreaView>
+)
+
+const LeftDrawerNav = createDrawerNavigator ({
+  Chat: {
+    screen: ChatScreen
+  },
+  Nest: {
+    screen: NestScreen
+  },
+}, {
+  contentComponent: CustomDrawerComponent,
+  contentOptions: {
+    activeTintColor: 'white',
+    inactiveTintColor: 'white',
+    activeBackgroundColor: '#3A4240',
+  },
+  drawerBackgroundColor: '#222A28',
+  drawerWidth: width * 0.6,
+  drawerPosition: 'left',
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e2e2e2',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ frontend/toucan/NestScreen.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Dimensions } from 'react-native';
+import { Icon, Button, Container, Header, Content, Left, Title, Body, Right } from 'native-base';
+
+// Below is some horribly ugly code that allows the header to render
+//    properly on the new iPhones. This was apparently fixed with 
+//    NativeBase version 2.9.2, but Expo and React Native don't
+//    play well together and using the latest version creates headaches...
+
+const { height } = Dimensions.get('window');
+
+topSpace = 0;
+headerSize = 64;
+
+if (height === 812) {
+  headerSize = 90;
+  topSpace = 0;
+} else if (height === 896) {
+  topSpace = 10;
+  headerSize = 90;
+} else if (height === 276) {
+  topSpace = 5;
+  headerSize = 90;
+} else {
+  topSpace = 0;
+  headerSize = 64;
+}
+
+export default class NestScreen extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Header iosBarStyle='light-content' style={styles.header}>
+          {/* <Container style={ styles.navButtons }> */}
+            <Left style={ styles.navButtons }>
+              <Icon name="ios-menu" onPress={() => this.props.navigation.openDrawer()} style={styles.icon} />
+            </Left>
+            <Body style={ styles.navButtons }>
+              <Title style={styles.navTitle}>Nest</Title>
+            </Body>
+            <Right style={ styles.navButtons }>
+              {/* empty */}
+            </Right>
+          {/* </Container> */}
+        </Header>
+        
+        <Content contentContainerStyle={styles.content}>
+          <Text>Nest Screen</Text>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    content: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#e8e8e8',
+    },
+    header: {
+      backgroundColor: '#1E7898',
+      height: headerSize,
+    },
+    icon: {
+        color: 'white',
+        left: 10,
+    },
+    navTitle: {
+      color: 'white',
+      fontSize: 21,
+      fontFamily: 'Ubuntu-B',
+    },
+    navButtons: {
+      flex: 1,
+      top: topSpace,
+    }
+  });
