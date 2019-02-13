a65d79115137ac7404c825f9797e80ecabbf1552
David Iordan <davidiordan@icloud.com>
2019-02-13 14:47:16 -0600

    Updated firebase config

--- frontend/toucan/src/Firebase/Config.js
+++ frontend/toucan/src/Firebase/Config.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCaCUXs9CBJNiFv3NI1LxWQzPrdvZyM58A",
-    authDomain: "toucantest-5ec71.firebaseapp.com",
-    databaseURL: "https://toucantest-5ec71.firebaseio.com",
-    projectId: "toucantest-5ec71",
-    storageBucket: "toucantest-5ec71.appspot.com",
-    messagingSenderId: "569196028022"
+    apiKey: "AIzaSyBQO-6Vual57ifhmg1WB7E7216BKh43r1M",
+    authDomain: "toucan-v1-6245e.firebaseapp.com",
+    databaseURL: "https://toucan-v1-6245e.firebaseio.com",
+    projectId: "toucan-v1-6245e",
+    storageBucket: "toucan-v1-6245e.appspot.com",
+    messagingSenderId: "7463834611"
 };
 
 firebase.initializeApp(firebaseConfig);
