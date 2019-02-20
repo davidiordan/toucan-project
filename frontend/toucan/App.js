8b7a89ff0c73db5a9b462ec1492f56fae7088ce9
David Iordan <davidiordan@icloud.com>
2019-02-20 15:04:59 -0600

    Moved the navigator code from App.js to Navigator.js

--- frontend/toucan/App.js
+++ frontend/toucan/App.js
@@ -4,13 +4,7 @@ import { createSwitchNavigator, createStackNavigator, createAppContainer, create
 import * as Expo from 'expo';
 
 import firebaseConfig from './src/Firebase/Config';
-
-import HomeScreen from './src/HomeScreen';
-import NestScreen from './src/NestScreen';
-import LoginScreen from "./src/Login/Login"
-import SignUpScreen from "./src/Login/SignUp";
-
-const { width } = Dimensions.get('window');
+import Navigator from './src/Navigator/Navigator';
 
 firebaseConfig;
 
@@ -33,59 +27,7 @@ export default class App extends React.Component {
       return <Expo.AppLoading />
     }
     return (
-      <AppCont />
+      <Navigator />
     );
   }
 }
-
-const CustomDrawerComponent = (props) => (
-  <SafeAreaView style={{flex: 1}}>
-    <ScrollView>
-      <DrawerItems {...props} />
-    </ScrollView>
-  </SafeAreaView>
-)
-
-const LeftDrawerNav = createDrawerNavigator ({
-  Home: {
-    screen: HomeScreen
-  },
-  Nest: {
-    screen: NestScreen
-  },
-}, {
-  contentComponent: CustomDrawerComponent,
-  contentOptions: {
-    activeTintColor: 'white',
-    inactiveTintColor: 'white',
-    activeBackgroundColor: '#3A4240',
-    labelStyle: { fontSize: 17, fontFamily: 'Ubuntu-B' },
-  },
-  drawerBackgroundColor: '#222A28',
-  drawerWidth: width * 0.6,
-  drawerPosition: 'left',
-})
-
-const AuthStack = createStackNavigator({ 
-  Login: LoginScreen,
-  SignUp: SignUpScreen,
-});
-
-const AppCont = createAppContainer(createSwitchNavigator(
-  {
-    App: LeftDrawerNav,
-    Auth: AuthStack,
-  },
-  {
-    initialRouteName: 'App', // edited here
-  }
-));
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e2e2e2',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ frontend/toucan/src/Navigator/Navigator.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, Dimensions } from 'react-native';
+import { createSwitchNavigator, createStackNavigator, createAppContainer, createDrawerNavigator, DrawerItems } from 'react-navigation';
+
+const { width } = Dimensions.get('window');
+
+import HomeScreen from '../HomeScreen';
+import NestScreen from '../NestScreen';
+import LoginScreen from "../Login/Login"
+import SignUpScreen from "../Login/SignUp";
+
+export default class Navigator extends React.Component {
+  render() {
+    return (<AppCont />);
+  }
+}
+
+const CustomDrawerComponent = (props) => (
+  <SafeAreaView style={{flex: 1}}>
+    <ScrollView>
+      <DrawerItems {...props} />
+    </ScrollView>
+  </SafeAreaView>
+)
+
+const LeftDrawerNav = createDrawerNavigator ({
+  Home: {
+    screen: HomeScreen
+  },
+  Nest: {
+    screen: NestScreen
+  },
+}, {
+  contentComponent: CustomDrawerComponent,
+  contentOptions: {
+    activeTintColor: 'white',
+    inactiveTintColor: 'white',
+    activeBackgroundColor: '#3A4240',
+    labelStyle: { fontSize: 17, fontFamily: 'Ubuntu-B' },
+  },
+  drawerBackgroundColor: '#222A28',
+  drawerWidth: width * 0.6,
+  drawerPosition: 'left',
+})
+
+const AuthStack = createStackNavigator({ 
+  Login: LoginScreen,
+  SignUp: SignUpScreen,
+});
+
+const AppCont = createAppContainer(createSwitchNavigator(
+  {
+    App: LeftDrawerNav,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'Auth', // edited here
+  }
+));
